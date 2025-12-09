32aec08b75548fde3682e59a740fe1da4d2f084a
Jeremy-Gstein <jeremy51b5@pm.me>
2025-12-09 15:35:35 -0500

    v1.0.0

--- WoWBnBCopy.lua
+++ WoWBnBCopy.lua
@@ -1,42 +1,57 @@
-local addonName = ...
-
--- Create main frame
-local frame = CreateFrame("Frame", "HelloWorldCopyFrame", UIParent, "BasicFrameTemplateWithInset")
-frame:SetSize(300, 120)
+-- Main frame
+local frame = CreateFrame("Frame", "WoWBnBCopyFrame", UIParent, "BasicFrameTemplateWithInset")
+frame:SetSize(400, 200)
 frame:SetPoint("CENTER")
 frame:Hide()
 
 frame.title = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
 frame.title:SetPoint("CENTER", frame.TitleBg, "CENTER", 0, 0)
-frame.title:SetText("Copy Text")
+frame.title:SetText("WoWBnB ~ Share your house with everyone!")
 
--- Edit box
-local editBox = CreateFrame("EditBox", nil, frame, "InputBoxTemplate")
-editBox:SetSize(260, 30)
-editBox:SetPoint("TOP", 0, -40)
-editBox:SetAutoFocus(false)
-editBox:SetText("Hello World")
+-- Multiline edit box
+local scrollFrame = CreateFrame("ScrollFrame", "WoWBnBCopyScrollFrame", frame, "UIPanelScrollFrameTemplate")
+scrollFrame:SetPoint("TOPLEFT", 16, -40)
+scrollFrame:SetPoint("BOTTOMRIGHT", -30, 40)
 
--- Copy button
-local copyButton = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
-copyButton:SetSize(80, 22)
-copyButton:SetPoint("BOTTOM", 0, 15)
-copyButton:SetText("Copy")
+local editBox = CreateFrame("EditBox", "WoWBnBCopyEditBox", scrollFrame)
+editBox:SetMultiLine(true)
+editBox:SetFontObject(ChatFontNormal)
+editBox:SetWidth(340)
+editBox:SetAutoFocus(true)
+editBox:SetText("How-to: 1. be on your house plot before running /wowbnb. 2. copy and share the command that will appear here")
 
-copyButton:SetScript("OnClick", function()
-    editBox:HighlightText()
-    editBox:SetFocus()
+scrollFrame:SetScrollChild(editBox)
+
+-- Close on Ctrl+C (similar to how /simc handles clipboard)
+editBox:SetScript("OnKeyDown", function(self, key)
+    if key == "C" and IsControlKeyDown() then
+        frame:Hide()
+    end
 end)
 
--- Slash command
-SLASH_HELLOWORLDCOPY1 = "/hw"
-SlashCmdList["HELLOWORLDCOPY"] = function()
+-- Okay button
+local okButton = CreateFrame("Button", "WoWBnBCopyOkButton", frame, "UIPanelButtonTemplate")
+okButton:SetSize(80, 24)
+okButton:SetPoint("BOTTOM", 0, 12)
+okButton:SetText("Okay")
+
+okButton:SetScript("OnClick", function()
+    frame:Hide()
+end)
+
+-- Slash command to toggle frame
+SLASH_WOWBNBCOPY1 = "/wowbnb"
+SLASH_WOWBNBCOPY2 = "/wbnb"
+SlashCmdList["WOWBNBCOPY"] = function()
     if frame:IsShown() then
         frame:Hide()
     else
         frame:Show()
-        editBox:SetText("Hello World")
+	houseInfo = C_Housing.GetCurrentHouseInfo()
+	-- /run C_Housing.VisitHouse(houseInfo.neighborehoodGUID, houseInfo.houseGUID, houseInfo.plotID)
+        editBox:SetText("/run C_Housing.VisitHouse(\"" .. houseInfo.neighborhoodGUID .. "\", \"" .. houseInfo.houseGUID .. "\", " .. houseInfo.plotID .. ")")
         editBox:HighlightText()
         editBox:SetFocus()
     end
 end
+
--- dev-build.sh
+++ dev-build.sh
@@ -7,16 +7,41 @@ addon_name="WoWBnB"
 addon="$addon_path/$addon_name"
 files="WoWBnBCopy.lua WoWBnB.toc" 
 
+# check if addon dir exists in WoW path.
 if [ ! -d "$addon" ]; then
 	mkdir -p "$addon"
 fi
 
-echo "Moving $files -> $addon"
-if cp $files "$addon"; then
-	ls -larth "$addon"
-	printf "\n\nDone! /rl to see changes in game." 
-	exit 0;
-else
-	echo "error copying files to addon folder.. check paths are correct"
-fi
+lazy_sync() {
+    echo "Moving $files -> $addon"
+    if cp $files "$addon"; then
+	    ls -larth "$addon"
+	    printf "\n\nDone! /rl to see changes in game." 
+	    exit 0;
+    else
+	    echo "error copying $files to $addon.. check paths are correct"
+    fi
+}
 
+lazy_rm() {
+    read -p "Delete $addon?? [y/N]: " choice
+    case "$choice" in
+	    y|Y) rm -rf "$addon" ;;
+	    *) exit 0 ;;
+    esac
+}
+
+
+# Loop through all args and allow combinations 
+# of multiple args when passed.
+for arg in "$@"; do
+  case "$arg" in
+    s|-s|sync|--sync) lazy_sync;;
+    rm|remove|--rm|--remove) lazy_rm ;;
+    *) echo "No arg passed default to sync" ;; 
+  esac
+done
+
+if [[ $# -eq 0 ]]; then
+  lazy_sync
+fi
